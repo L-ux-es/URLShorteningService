4d7a136cc8ce10e0f68c5ee607134fa2ad9ff9ba
Leo <leo@>
2025-10-26 19:22:54 -0400

    ci: Crear .env

--- src/main/java/com/luxes/UrlShorteningServiceApplication.java
+++ src/main/java/com/luxes/UrlShorteningServiceApplication.java
@@ -3,12 +3,30 @@ package com.luxes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 @SpringBootApplication
 public class UrlShorteningServiceApplication {
 
-
     public static void main(String[] args) {
+        loadEnvVariables();
         SpringApplication.run(UrlShorteningServiceApplication.class, args);
     }
 
+    private static void loadEnvVariables() {
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(".env"));
+            for (String line : lines) {
+                String[] parts = line.split("=", 2);
+                if (parts.length == 2) {
+                    System.setProperty(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
